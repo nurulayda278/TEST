eeb7fe81d990a3a9fc02cd4605480e04c6df1c26
nurulayda278 <nurulayda278@gmail.com>
2026-07-05 17:01:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const API_KEY = "curl -X GET 'https://www.goldapi.io/api/XAU/USD' 
-     -H 'x-access-token: YOUR_API_KEY";
+const API_KEY = "GET https://api.metals.dev/v1/latest
+
+{
+    "status": "success",
+    "currency": "USD",
+    "unit": "toz",
+    "metals": {
+        "gold": 1929.15,
+        "silver": 22.99,
+        "platinum": 921.398,
+        "lbma_gold_am": 1913.75,
+        "lbma_gold_pm": 1928.75,
+        [28+ precious & industrial metal prices]
+    },
+    "currencies": {
+        "GBP": 1.26962,
+        "EUR": 1.08955,
+        "JPY": 0.006925,
+        [170+ currency conversion rates]
+    },
+    "timestamp": "2023-07-04T09:01:04.744Z"
+}";
 
 async function getGoldPrice() {
   try {
