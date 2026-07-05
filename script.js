3413c7b95777f9551deb0a1412af59da74c75629
nurulayda278 <nurulayda278@gmail.com>
2026-07-05 17:27:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const API_KEY = "https://api.metalpriceapi.com/v1/latest ?api_key=478409b6bf23e895e5d8bbc1a0930776 &base=USD &currencies=EUR,XAU,XAG";
+const API_KEY = "478409b6bf23e895e5d8bbc1a0930776";
 
-async function getGoldPrice() {
+async function getMetalPrice() {
   try {
-    const response = await fetch("https://www.goldapi.io/api/XAU/USD", {
-      headers: {
-        "x-access-token": API_KEY,
-        "Content-Type": "application/json"
-      }
-    });
+    const url = `https://api.metalpriceapi.com/v1/latest?api_key=${API_KEY}&base=USD&currencies=EUR,XAU,XAG`;
 
+    const response = await fetch(url);
     const data = await response.json();
 
     console.log(data);
@@ -18,4 +14,4 @@ async function getGoldPrice() {
   }
 }
 
-getGoldPrice();
+getMetalPrice();
